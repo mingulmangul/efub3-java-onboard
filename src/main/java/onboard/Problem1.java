8a3aa5a9904b7d878ff1d58d19bf17692ecebe83
Min A <min2010011@gmail.com>
2023-03-15 22:56:16 +0900

    feat: Problem1 풀이 추가

--- src/main/java/onboard/Problem1.java
+++ src/main/java/onboard/Problem1.java
@@ -4,8 +4,20 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Problem1 {
-    public static List<Integer> solution(Integer hour, Integer minute){
+    public static List<Integer> solution(Integer hour, Integer minute) {
         List<Integer> answer = new ArrayList<>();
+        // 단위를 분으로 바꿔서 45분 빼기
+        int time = hour * 60 + minute - 45;
+        int nHour, nMin;
+        if (time < 0) { // 45분 앞선 시간이 전날이 되는 경우
+            nHour = 23;
+            nMin = 60 + time;
+        } else {
+            nHour = time / 60;
+            nMin = time % 60;
+        }
+        answer.add(nHour);
+        answer.add(nMin);
         return answer;
     }
 }
